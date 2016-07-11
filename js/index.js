c641bf160eae6d4c91c4aa8b9439ecf49a873ff9
Luc lambert <lucthehuman21@gmail.com>
2016-07-11 11:47:28 -1000

    tons of js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,28 @@
-var b
 var responses = 0
+var mostDemonPoints = 0
 var press = function() {
 	responses++;
 	document.getElementById('score').innerHTML =  + responses + " said that it was crowded";
-	if (demonPoints > mostDemonPoints) {
-		document.getElementById('highscore').innerHTML = "New high score: " + demonPoints + " points";
+	if (responses > mostDemonPoints) {
+		document.getElementById('highscore').innerHTML = + responses + " said that it was crowded";
+	}
+}
+var responses2 = 0
+var mostDemonPoints2 = 0
+var press = function() {
+	responses2++;
+	document.getElementById('score').innerHTML =  + responses2 + " said that it was normal";
+	if (responses2 > mostDemonPoints2) {
+		document.getElementById('highscore').innerHTML = + responses2 + " said that it was normal";
+	}
+}
+var responses3 = 0
+var mostDemonPoints3 = 0
+var press = function() {
+	responses3++;
+	document.getElementById('score').innerHTML =  + responses3 + " said that it was deserted";
+	if (responses3 > mostDemonPoints3) {
+		document.getElementById('highscore').innerHTML = + responses3 + " said that it was deserted";
 	}
 }
 
@@ -14,3 +32,11 @@ var rest1 = function(){
 	document.getElementById("buttons").innerHTML =
 		"<buttons onclick='rest1low()'>Not Crowded</buttons><buttons onclick='rest1low()'>Not Crowded</buttons><buttons onclick='rest1low()'>Not Crowded</buttons>"
 }
+
+var loop = function(){
+	for (var i = 0; i < 10; i++) {
+		console.log("The number is "+i);
+	}
+}
+
+loop();
