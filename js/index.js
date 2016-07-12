629f02a1d2dfad12aa699d4a118eea40cb59e2bd
Luc lambert <lucthehuman21@gmail.com>
2016-07-12 11:09:31 -1000

    added tons
    got way more java script and then got some html, and took out html
    buttons

--- js/index.js
+++ js/index.js
@@ -1,42 +1,29 @@
+var arr{"it is crowded", "it is normal", "it is empty"}
 var responses = 0
-var mostDemonPoints = 0
-var press = function() {
+var mostDemonPoints = 0 
+var restlow = function() {
 	responses++;
 	document.getElementById('score').innerHTML =  + responses + " said that it was crowded";
-	if (responses > mostDemonPoints) {
-		document.getElementById('highscore').innerHTML = + responses + " said that it was crowded";
-	}
 }
 var responses2 = 0
 var mostDemonPoints2 = 0
-var press = function() {
+var restmid = function() {
 	responses2++;
 	document.getElementById('score').innerHTML =  + responses2 + " said that it was normal";
-	if (responses2 > mostDemonPoints2) {
-		document.getElementById('highscore').innerHTML = + responses2 + " said that it was normal";
-	}
+	
 }
 var responses3 = 0
 var mostDemonPoints3 = 0
-var press = function() {
+var resthigh = function() {
 	responses3++;
 	document.getElementById('score').innerHTML =  + responses3 + " said that it was deserted";
-	if (responses3 > mostDemonPoints3) {
-		document.getElementById('highscore').innerHTML = + responses3 + " said that it was deserted";
-	}
 }
 
-
+onclick(){getElementById arr 0}
 var rest1 = function(){
-	document.getElementById("title").innerHTML = "A Place to Eat";
+	document.getElementById("title").innerHTML = "Highway Inn";
 	document.getElementById("buttons").innerHTML =
-		"<buttons onclick='rest1low()'>Not Crowded</buttons><buttons onclick='rest1low()'>Not Crowded</buttons><buttons onclick='rest1low()'>Not Crowded</buttons>"
+		"<buttons onclick='restlow(one)'>Not Crowded</buttons><buttons onclick='restmid(two)'></buttons><buttons onclick='resthigh(three)'>crowded</buttons>"
 }
 
-var loop = function(){
-	for (var i = 0; i < 10; i++) {
-		console.log("The number is "+i);
-	}
-}
 
-loop();
