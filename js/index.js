3c1a9cf6db7ee83b75714c69ad988894df0224e5
Luc lambert <lucthehuman21@gmail.com>
2016-07-12 11:52:47 -1000

    added tons of more js
    we did some minor fixes for html, and charles helped us with the
    javascript and made it so that the buttons worked and showed us what to
    do to add more restaurants

--- js/index.js
+++ js/index.js
@@ -1,29 +1,48 @@
-var arr{"it is crowded", "it is normal", "it is empty"}
+var arr= ["it is crowded", "it is normal", "it is empty"];
+
+var one = {
+	relow: 0,
+	remid: 0,
+	rehigh: 0
+};
+var two = {
+	relow: 0,
+	remid: 0,
+	rehigh: 0
+};
+var three = {
+	relow: 0,
+	remid: 0,
+	rehigh: 0
+};
+
 var responses = 0
 var mostDemonPoints = 0 
-var restlow = function() {
-	responses++;
-	document.getElementById('score').innerHTML =  + responses + " said that it was crowded";
+var restlow = function(re) {
+	re.relow++
+
+	//responses++;
+	document.getElementById('results').innerHTML = re.relow + " said that it was crowded";
 }
 var responses2 = 0
 var mostDemonPoints2 = 0
-var restmid = function() {
+var restmid = function(re) {
 	responses2++;
-	document.getElementById('score').innerHTML =  + responses2 + " said that it was normal";
+	document.getElementById('results').innerHTML = responses2 + " said that it was normal";
 	
 }
 var responses3 = 0
 var mostDemonPoints3 = 0
-var resthigh = function() {
+var resthigh = function(re) {
 	responses3++;
-	document.getElementById('score').innerHTML =  + responses3 + " said that it was deserted";
+	document.getElementById('results').innerHTML = responses3 + " said that it was deserted";
 }
 
-onclick(){getElementById arr 0}
 var rest1 = function(){
 	document.getElementById("title").innerHTML = "Highway Inn";
 	document.getElementById("buttons").innerHTML =
-		"<buttons onclick='restlow(one)'>Not Crowded</buttons><buttons onclick='restmid(two)'></buttons><buttons onclick='resthigh(three)'>crowded</buttons>"
+		"<button onclick='restlow(one)'>crowded</button><button onclick='restmid(one)'>Medium</button><button onclick='resthigh(one)'>not crowded</button>";
+	document.getElementById("results").innerHTML = one.relow+"said it was not crowded";
 }
 
 
